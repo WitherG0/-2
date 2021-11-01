ecf4f7c425c3d1620f5d485bcc6e52bec632d3bd
WitherG0 <91891123+WitherG0@users.noreply.github.com>
2021-11-01 12:24:59 +0300

    Update Program.cs

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Six
 {
@@ -6,20 +6,28 @@ namespace Six
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите возраст (в годах)");
-            string text = Console.ReadLine();
-            Console.WriteLine("|||||||||||||||||||||||||||");
-            int y = text[text.Length - 1] - '0';
-            int a = int.Parse(text);
-            if (y == 1 && a != 11) 
-            { 
-                Console.WriteLine($"{text} год"); 
-            }else if (y == 2 || y == 3 || y == 4 && a != 12 && a != 13 && a != 14) 
+            for (int i = 0; i <= 100; i++)
             {
-                Console.WriteLine($"{text} годa"); 
-            } else 
-            { 
-                Console.WriteLine($"{text} лет"); 
+                Console.WriteLine("Введите возраст (в годах)");
+                string text = $"{i}";
+                Console.WriteLine("|||||||||||||||||||||||||||");
+                int y = text[text.Length - 1] - '0';
+                int a = int.Parse(text);
+                if (y == 1 && a != 11)
+                {
+                    Console.WriteLine($"{text} год");
+                }
+                else if (a == 12 || a == 13 || a == 14)
+                    {
+                    Console.WriteLine($"{text} лет");
+                    }
+                    else if (y == 2 || y == 3 || y == 4)
+                    {
+                    Console.WriteLine($"{text} года");
+                    } else
+                {
+                    Console.WriteLine($"{text} лет");
+                }
             }
         }
     }
